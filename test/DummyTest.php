d1e298ac35e88ca099dae2a9bc2840864827d491
Rubén García <rubengarcia@grupojuinsa.es>
2020-02-03 13:17:56 +0100

    Initial commit skeleton

---
+++ test/DummyTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    /** @test */
+    public function
+    dummy_test()
+    {
+        $this->assertTrue(true);
+    }
+}
